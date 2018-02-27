2bd73787d28a87adbc1494f0789b47727d98eb01
Chris Gatewood <c.gatewood55@gmail.com>
2018-02-26 20:42:33 -0600

    challenge 3

--- strings.js
+++ strings.js
@@ -5,3 +5,8 @@ var numOfSandwhiches = 4*2;
  var name= "christian";
  var string = "Hello, "+ name + " how are you doing today?"
  console.log("string",string);
+
+ // challenge3
+  var DNA= "GCAT"
+  var RNA= DNA.replace('T','U');
+  console.log("RNA",RNA);
