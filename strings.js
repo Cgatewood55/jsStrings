677c860bdcec41c844f386ee1a55602b63474e41
Chris Gatewood <c.gatewood55@gmail.com>
2018-02-26 20:31:08 -0600

    challenge 2

--- strings.js
+++ strings.js
@@ -1,2 +1,7 @@
 var numOfSandwhiches = 4*2;
-console.log("numOfSandwhiches", numOfSandwhiches);
+ console.log("numOfSandwhiches", numOfSandwhiches);
+
+// challenge 2
+ var name= "christian";
+ var string = "Hello, "+ name + " how are you doing today?"
+ console.log("string",string);
