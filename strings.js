682b9a943e6715e2d9626708736171c887088306
Chris Gatewood <c.gatewood55@gmail.com>
2018-02-26 21:13:44 -0600

    challenge 4

--- strings.js
+++ strings.js
@@ -10,3 +10,12 @@ var numOfSandwhiches = 4*2;
   var DNA= "GCAT"
   var RNA= DNA.replace('T','U');
   console.log("RNA",RNA);
+
+  //challenge 4
+  //var animal = "Goat";
+  var animal = "AlliGATor";
+  if (animal.toLowerCase()=== "alligator"){
+      console.log("small");
+  } else {
+      console.log("wide");
+  }
